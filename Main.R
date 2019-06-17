8b4b02a0d0bb5f01d4cac20d6cad988613f46c6c
ian@gledhills.mobi <ian@gledhills.mobi>
2019-06-17 08:30:04 +0100

    Runs but no meaningful predictions

--- Main.R
+++ Main.R
@@ -17,7 +17,7 @@ if(!require(scales)) install.packages("scales", repos = "http://cran.us.r-projec
 
 library(tidyverse)
 library(caret)
-
+library(rpart.plot)
 
 multiMerge = function(filePattern, nrows){
   
@@ -49,29 +49,42 @@ multiMerge = function(filePattern, nrows){
                 })
   
   Reduce(function(x,y) {merge(x, y, all = TRUE)}, datalist)
+
 }
 
-flights <- multiMerge("2018.*zip", 150000)
-flights <- flights[sample(nrow(flights), 30000),]
+flights <- multiMerge("2018.*zip", 15000)
+flights <- flights[sample(nrow(flights), 30000),] %>%
+            mutate(CANCELLED = as.logical(CANCELLED))
+flights <- flights %>%
+            mutate(
+              ACTIVE = ifelse(!is.na(DEP_DELAY), TRUE, FALSE),
+              DEP_STATUS = as.factor(case_when(
+                  flights$CANCELLED ~ "CANCELLED", 
+                  flights$DEP_DELAY_GROUP < 0 ~ "EARLY",
+                  flights$DEP_DELAY_GROUP == 0 ~ "ONTIME",
+                  flights$DEP_DELAY_GROUP > 0 ~ "DELAYED",
+                  TRUE ~ "UNKNOWN"))
+            )
 
-names(flights$DAY_OF_WEEK) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Unknown")
+levels(flights$DAY_OF_WEEK) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Unknown")
 
-cancelled_flights <- flights[is.na(flights$DEP_DELAY),]
-active_flights <- flights[!is.na(flights$DEP_DELAY),]
+#cancelled_flights <- flights[flights$DEP_STATUS == "CANCELLED",]
+#active_flights <- flights[flights$DEP_STATUS != "CANCELLED",]
 
-rm(flights)
+# rm(flights)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Partitions the supplied data into a test and train data set using the 
 # supplied percentage to the training data set.
 #_____________________________________________________________________
 
-set.seed(755)
-test_index <- createDataPartition(y = active_flights$DEP_DELAY, times = 1,
-                                  p = 0.2, list = FALSE)
+test_index <- createDataPartition(y = flights$DEP_STATUS, 
+                                  times = 1,
+                                  p = 0.2, 
+                                  list = FALSE)
 
-train_set <- active_flights[-test_index,]
-test_set <- active_flights[test_index,]
+train_set <- flights[-test_index,]
+test_set <- flights[test_index,]
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -82,9 +95,10 @@ test_set <- active_flights[test_index,]
 predictor_columns <- c("MONTH", "ORIGIN_STATE_ABR", "DISTANCE", "DAY_OF_MONTH", "DEP_TIME_BLK", "DAY_OF_WEEK", "OP_UNIQUE_CARRIER")
 predictor_columns <- c("DAY_OF_WEEK")
 predictor_columns <- c("MONTH", "ORIGIN_STATE_ABR", "DISTANCE", "DAY_OF_MONTH", "DEP_TIME_BLK", "DAY_OF_WEEK", "OP_UNIQUE_CARRIER")
-predictor_columns <- c("DEP_TIME_BLK", "DAY_OF_WEEK", "OP_UNIQUE_CARRIER")
+predictor_columns <- c("DEP_TIME_BLK", "DAY_OF_WEEK", "OP_UNIQUE_CARRIER", "ORIGIN")
+predictor_columns <- c("DEP_TIME_BLK", "DAY_OF_WEEK")
 
-y_train<-train_set[,"DEP_DELAY"] 
+y_train<-train_set[,"DEP_STATUS"] 
 x_train<-train_set[, predictor_columns]
 
 train_df<-data.frame(x=x_train,y=y_train)
@@ -96,6 +110,9 @@ train_df<-data.frame(x=x_train,y=y_train)
 library(rpart)
 
 train_rpart <- train(y ~ ., method="rpart", tuneGrid=data.frame(cp = seq(0, 0.2, len=10)), data = train_df)
+
+predictions <- train_set %>% 
+  mutate(y_hat = predict(train_rpart))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Visualizer helpers
